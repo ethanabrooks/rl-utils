c14b35d0760d6c7a3f64590a296005042fbf78f3
Ethan Brooks <ethanabrooks@gmail.com>
2018-11-19 23:59:23 -0500

    moved stuff aroung

--- utils/arguments.py
+++ utils/arguments.py
@@ -3,6 +3,7 @@ import re
 import tempfile
 from collections.__init__ import namedtuple
 from contextlib import contextmanager
+from itertools import filterfalse
 from pathlib import Path
 from typing import Tuple, List
 from xml.etree import ElementTree as ET
@@ -12,21 +13,12 @@ import tensorflow as tf
 from gym import spaces
 from gym.spaces import Box
 
-from environments.hindsight_wrapper import HSRHindsightWrapper, MBHSRHindsightWrapper
-from environments.hsr import MultiBlockHSREnv, HSREnv, MoveGripperEnv
 from sac.utils import parametric_relu
 
 
 def make_box(*tuples: Tuple[float, float]):
     low, high = map(np.array, zip(*[(map(float, m)) for m in tuples]))
     return spaces.Box(low=low, high=high, dtype=np.float32)
-
-
-ENVIRONMENTS = dict(
-    multi_block=MultiBlockHSREnv,
-    move_block=HSREnv,
-    move_gripper=MoveGripperEnv,
-)
 
 
 def parse_space(dim: int):
@@ -56,10 +48,6 @@ def cast_to_int(arg: str):
     return int(float(arg))
 
 
-HINDSIGHT_ENVS = {
-    HSREnv: HSRHindsightWrapper,
-    MultiBlockHSREnv: MBHSRHindsightWrapper,
-}
 ACTIVATIONS = dict(
     relu=tf.nn.relu,
     leaky=tf.nn.leaky_relu,
@@ -179,3 +167,24 @@ def mutate_xml(changes: List[XMLSetter], dofs: List[str], goal_space: Box, n_blo
     finally:
         for f in temp.values():
             f.close()
+
+
+def parse_groups(parser: argparse.ArgumentParser):
+    args = parser.parse_args()
+
+    def is_optional(group):
+        return group.title == 'optional arguments'
+
+    def parse_group(group):
+        # noinspection PyProtectedMember
+        return {a.dest: getattr(args, a.dest, None) for a in group._group_actions}
+
+    # noinspection PyUnresolvedReferences,PyProtectedMember
+    groups = [g for g in parser._action_groups if g.title != 'positional arguments']
+    optional = filter(is_optional, groups)
+    not_optional = filterfalse(is_optional, groups)
+
+    kwarg_dicts = {group.title: parse_group(group) for group in not_optional}
+    kwargs = (parse_group(next(optional)))
+    del kwargs['help']
+    return {**kwarg_dicts, **kwargs}
