e0cae39210681581cc5115e6a1e340bbe1596fb5
Ethan Brooks <ethanabrooks@gmail.com>
2018-11-19 23:45:44 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+# third party
+from setuptools import find_packages, setup
+
+setup(
+    name='utils',
+    version='0.0.0',
+    author='Ethan Brooks',
+    author_email='ethanabrooks@gmail.com',
+    packages=find_packages(),
+    )
