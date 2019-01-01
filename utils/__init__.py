a3182501cd79beea2f49e1dbc01423ce034b1e6f
Ethan Brooks <ethanabrooks@gmail.com>
2019-01-01 16:07:05 -0500

    re-add flattened imports to __init__

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+from utils.argparse import *
+from utils.array_group import *
+from utils.gym import *
+from utils.numpy import *
+from utils.tensorflow import *
+from utils.types import *
+from utils.replay_buffer import *
