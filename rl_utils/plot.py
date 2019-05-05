ace958010df411baf8aaca75b7082ce39adb0794
Ethan Brooks <ethanabrooks@gmail.com>
2019-05-05 17:00:22 -0400

    add --tags to plot

--- rl_utils/plot.py
+++ rl_utils/plot.py
@@ -18,7 +18,8 @@ def cli():
     parser.add_argument('--names', nargs='*', type=Path)
     parser.add_argument('--paths', nargs='*', type=Path)
     parser.add_argument('--base-dir', default='.runs/logdir', type=Path)
-    parser.add_argument('--tag', default='return')
+    parser.add_argument('--tag')
+    parser.add_argument('--tags', nargs='*')
     parser.add_argument('--quiet', action='store_true')
     parser.add_argument('--limit', type=int)
     parser.add_argument('--fname', type=str, default='plot')
@@ -30,14 +31,25 @@ def cli():
 def main(
         names: List[str],
         paths: List[Path],
+        tags: List[str],
         tag: str,
         base_dir: Path,
         limit: Optional[int],
         quiet: bool,
         **kwargs,
 ):
+    if tags and tag:
+        raise RuntimeError('use either --tag or --tags, not both')
+    if tag:
+        tags = [tag] * len(names)
+
+    if not (len(names) == len(paths) == len(tags)):
+        raise RuntimeError(
+            f'These three values should have the same number of values:\nnames: ({names})\npaths: ({paths})\ntags: ({tags})'
+        )
+
     def get_tags():
-        for name, path in zip(names, paths):
+        for name, path, tag in zip(names, paths, tags):
             path = Path(base_dir, path)
             if not path.exists():
                 if not quiet:
@@ -54,8 +66,9 @@ def main(
                             if limit is None or event.step < limit:
                                 yield event.step, value, name
 
-    data = pd.DataFrame(get_tags(), columns=['step', tag, 'run'])
-    sns.lineplot(x='step', y=tag, hue='run', data=data)
+    header = tags[0] if len(tags) == 1 else ''
+    data = pd.DataFrame(get_tags(), columns=['step', header, 'run'])
+    sns.lineplot(x='step', y=header, hue='run', data=data)
     plt.legend(data['run'].unique())
     plt.axes().ticklabel_format(style='sci', scilimits=(0, 0), axis='x')
     plt.savefig(**kwargs)
