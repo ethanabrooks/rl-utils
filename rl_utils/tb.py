6a80b066bf2f3626fc937d1c85862cf6548ee204
Ethan Brooks <ethanabrooks@gmail.com>
2019-12-24 18:03:38 -0500

    use --bind_all in tb tensorboard command

--- rl_utils/tb.py
+++ rl_utils/tb.py
@@ -12,8 +12,9 @@ def cmd(args, print_cmd=False, fail_ok=False, cwd=None):
         stderr=subprocess.PIPE,
         stdout=subprocess.PIPE,
         cwd=cwd,
-        universal_newlines=True)
-    cmd_string = ' '.join(args)
+        universal_newlines=True,
+    )
+    cmd_string = " ".join(args)
     if print_cmd:
         print(cmd_string)
     stdout, stderr = process.communicate(timeout=1)
@@ -24,29 +25,31 @@ def cmd(args, print_cmd=False, fail_ok=False, cwd=None):
 
 def cli():
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('port', type=int)
-    parser.add_argument('path', type=Path)
-    parser.add_argument('--logdir', type=Path, default=Path('.runs/logdir'))
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("port", type=int)
+    parser.add_argument("path", type=Path)
+    parser.add_argument("--logdir", type=Path, default=Path(".runs/logdir"))
     tb(**vars(parser.parse_args()))
 
 
 def tb(port: int, path: Path, logdir: Path):
-    active_sessions = cmd('tmux ls -F #{session_name}'.split())
-    session_name = f'tensorboard{port}'
+    active_sessions = cmd("tmux ls -F #{session_name}".split())
+    session_name = f"tensorboard{port}"
     logdir = Path(logdir, path)
     if not logdir.exists():
-        raise RuntimeError(f'Path {logdir} does not exist.')
-    command = f'tensorboard --logdir={logdir} --port={port}'
+        raise RuntimeError(f"Path {logdir} does not exist.")
+    command = f"tensorboard --logdir={logdir} --port={port} --bind_all"
     if session_name in active_sessions:
-        window_name = f'{session_name}:0'
-        cmd(['tmux', 'respawn-window', '-t', window_name, '-k', command],
-                print_cmd=True)
-        print(f'Respawned {window_name} window.')
+        window_name = f"{session_name}:0"
+        cmd(
+            ["tmux", "respawn-window", "-t", window_name, "-k", command], print_cmd=True
+        )
+        print(f"Respawned {window_name} window.")
     else:
-        cmd(['tmux', 'new', '-d', '-s', session_name, command], print_cmd=True)
-        print(f'Created new session called {session_name}.')
+        cmd(["tmux", "new", "-d", "-s", session_name, command], print_cmd=True)
+        print(f"Created new session called {session_name}.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
